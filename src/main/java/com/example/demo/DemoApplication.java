5f6443a0b4e08c48f0b35452882cf30b5829a710
Deepanshu Kumar <anshuk6469@gmail.com>
2024-04-09 15:53:04 +0530

    done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    private static String names;
+
     @Value("${NAMES}")
-	static String names;
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hello "+ names);
-	}
+    public void setNames(String names) {
+        DemoApplication.names = names;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Hello " + names);
+    }
 }
