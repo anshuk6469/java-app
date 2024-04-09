1c1a0a5a24d8951352255d15baa252e00887b3de
Deepanshu Kumar <anshuk6469@gmail.com>
2024-04-09 15:49:38 +0530

    done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     @Value("${NAMES}")
-	String names;
+	static String names;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello "+ names);
